2a7bb472ca9adf2f334bed679ab8b468e51776df
早田 武央 <soda.tk@commerce-gate.local>
2020-01-04 02:44:59 +0900

    modify indexview

--- polls/views.py
+++ polls/views.py
@@ -15,9 +15,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
     
     def get_queryset(self):
-        return Question.objects\
+        return Question.objects.order_by('-pub_date')[:5]
             #.filter(pubDate__lte=timezone.now())\
-            .order_by('-pub_date')[:5]
+            
     
 class DetailView(generic.DetailView):
     model = Question
