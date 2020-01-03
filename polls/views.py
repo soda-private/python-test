0f84d1fbfa7f82002a8d5fdca8292a19771fc53c
早田 武央 <soda.tk@commerce-gate.local>
2020-01-04 02:06:43 +0900

    test add

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from django.shortcuts import get_object_or_404
 
@@ -14,7 +15,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
     
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects\
+            .filter(pubDate__lte=timezone.now())\
+            .order_by('-pub_date')[:5]
     
 class DetailView(generic.DetailView):
     model = Question
